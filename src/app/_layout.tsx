2f50e0b39af008e69bd5d2a7004be5dcd6344709
Neilson Gamble <neilgamb@gmail.com>
2023-02-27 12:14:29 -0700

    moving app code into /src dir

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -37,7 +37,7 @@ function RootLayoutNav() {
     <>
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: "modal" }} />
+        {/* <Stack.Screen name="modal" options={{ presentation: "modal" }} /> */}
       </Stack>
     </>
   );
